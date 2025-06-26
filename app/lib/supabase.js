71d9be2015dd870b94cfd6a32bcc2883fc022282
arslan19970000 <167683066+arslan19970000@users.noreply.github.com>
2025-06-26 22:45:22 +0500

    Update supabase.js

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,5 +1,6 @@
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl="https://whqqsnxighjeqqhhithe.supabase.co"
-const supabaseAnonKey="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndocXFzbnhpZ2hqZXFxaGhpdGhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4ODc1NTEsImV4cCI6MjA2NTQ2MzU1MX0.6o9eE9h4gNqdBseBrzgRSj800t8B5Cc0INuPMFlt_G0"
-export const supabase = createClient(supabaseUrl ,supabaseAnonKey)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
